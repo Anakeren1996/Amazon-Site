324db7e6b591a39e430f88ba4779a547c47e0e82
Ana <keeren.1996@gmail.com>
2023-08-02 16:02:55 -0300

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,58 +1,72 @@
 // ALL THE CODES ITS RELATED TO THE CART
 
-export let cart = [{
-  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity: 2,
-}, {
-  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  quantity: 1
-}];
+export let cart = JSON.parse(localStorage.getItem("cart"));
+
+if (!cart) {
+  cart = [
+    // {
+    //   productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    //   quantity: 2,
+    // },
+    // {
+    //   productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    //   quantity: 1,
+    // },
+  ];
+}
+
+function saveToStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
-    let matchingItem;
-  
-    // Check if this product name is already in the cart
-    // We'll loop through the cart
-    cart.forEach((cartItem) => {
-      if (productId === cartItem.productId) {
-        matchingItem = cartItem;
-      }
-  
-      // VER ESSE CODIGO AQUI DE ADDEDMESSAGE DEPOIS
-      // const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
-    });
-  
-    // Adding products to the cart by the quantity selected in the select
-    const quantitySelector = document.querySelector(
-      `.js-quantity-selector-${productId}`
-    );
-  
-    const quantity = Number(quantitySelector.value);
-  
-    if (matchingItem) {
-      matchingItem.quantity += quantity;
-    } else {
-      cart.push({
-        productId: productId,
-        quantity: quantity,
-      });
+  let matchingItem;
+
+  // Check if this product name is already in the cart
+  // We'll loop through the cart
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
     }
-  }
 
-  // FUNCTION DELETE STEPS
-  // 1- CREATE A NEW ARRAY
-  // 2 - LOOP THROUGH THE CART
-  // 3 - ADD EACH PRODUCT TO THE NEW ARRAY, EXCEPT FOR THIS PRODUCTID
+    // VER ESSE CODIGO AQUI DE ADDEDMESSAGE DEPOIS
+    // const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
+  });
 
- export function removeFromCart(productId) {
-    const newCart = [];
+  // Adding products to the cart by the quantity selected in the select
+  const quantitySelector = document.querySelector(
+    `.js-quantity-selector-${productId}`
+  );
 
-    cart.forEach((cartItem) => {
-      if(cartItem.productId !== productId) {
-        newCart.push(cartItem);
-      }
+  const quantity = Number(quantitySelector.value);
+
+  if (matchingItem) {
+    matchingItem.quantity += quantity;
+  } else {
+    cart.push({
+      productId: productId,
+      quantity: quantity,
     });
-
-    cart = newCart;
   }
-  
+
+  saveToStorage();
+}
+
+// FUNCTION DELETE STEPS
+// 1- CREATE A NEW ARRAY
+// 2 - LOOP THROUGH THE CART
+// 3 - ADD EACH PRODUCT TO THE NEW ARRAY, EXCEPT FOR THIS PRODUCTID
+
+export function removeFromCart(productId) {
+  const newCart = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
+
+  saveToStorage();
+}
